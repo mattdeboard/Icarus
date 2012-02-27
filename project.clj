dd7a17cb155749828eb8620450b27e6b9f0c2690
Matt DeBoard <matt.deboard@gmail.com>
2012-02-27 11:15:37 -0500

    Change version # of clojure-solr dep.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject icarus "0.1a"
   :description "A robust Solr interface."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.mattdeboard/clojure-solr "0.4.0-SNAPSHOT"]]
+                 [org.clojars.mattdeboard/clojure-solr "0.4.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]]
   :main icarus.core)
